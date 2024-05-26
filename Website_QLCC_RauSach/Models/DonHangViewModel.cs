cceebbee3d65c8ab9110cf975abbe573e6e6f106
Trương Hồng Anh <honganh200307@gmail.com>
2024-05-26 14:41:16 +0700

    ListofOrder_DetailOrder_EditOrderStatus

---
+++ Website_QLCC_RauSach/Models/DonHangViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Website_QLCC_RauSach.Models
+{
+	public class DonHangViewModel
+	{
+        public IEnumerable<DonHang> TatCaDonHang { get; set; }
+        public IEnumerable<DonHang> ChoXacNhanDonHang { get; set; }
+		public IEnumerable<DonHang> DangGiaoDonHang { get; set; }
+		public IEnumerable<DonHang> DaGiaoDonHang { get; set; }
+		public IEnumerable<DonHang> DaHuyDonHang { get; set; }
+	}
+}
